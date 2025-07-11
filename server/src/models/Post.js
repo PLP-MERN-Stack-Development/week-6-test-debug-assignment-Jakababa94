0f476a56bdd07be013edb2405a6d8acfc3395f75
Jakababa94 <jakababa002@gmail.com>
2025-07-11 16:19:07 +0300

    Updated modules

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -1,9 +1,11 @@
 const mongoose = require ('mongoose');
 
-const PostSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
     tittle: { type: String, required: [true, 'Please provide a title']},
     content: { type: String, required: [true, 'Please provide content']},
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,},
-    category: { type: mongoose.Types.ObjectId(), required: true },
+    category: { type: String, required: true },
     slug: { type: String, unique: true, },
 })
+
+module.exports = mongoose.model('Post', postSchema);
